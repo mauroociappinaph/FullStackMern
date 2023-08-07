199646cae456a2c49c11562352745a0c686a1786
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-07 20:43:37 -0300

    Creación del Servidor

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
-import express from "express"
+import express from "express";
+
+const app = express();
+
+app.use("/", (req, res) => {
+res.send("Hola Mundo") ;
+
+});
+
+app.listen(4000, () => {
+  console.log("Servidor corriendo en puerto 4000. Happy Coding!");
+});
