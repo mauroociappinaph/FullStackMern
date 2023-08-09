a21a5b58569ff21a73de4792b52d732a7c3e26e1
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-09 15:38:16 -0300

    MiddlewareAuthentication

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -27,3 +27,18 @@ const checkAuth = async (req, res, next) => {
 };
 
 export default checkAuth;
+
+/*
+Este fragmento de código define una función de middleware asincrónica llamada
+checkAuth. Se utiliza para autenticar las solicitudes en una API.
+La función verifica si la solicitud tiene un encabezado de autorización con un token Bearer.
+Si lo tiene, verifica el token utilizando una biblioteca JWT, recupera la información
+del usuario de una base de datos y la adjunta al objeto de solicitud. 
+Luego, llama a la función next para pasar el control al siguiente middleware.
+Si el token es inválido o hay un error durante el proceso de verificación, 
+devuelve una respuesta 403 Forbidden con un mensaje de error.
+Si no hay ningún token en el encabezado de la solicitud, devuelve una respuesta 
+403 Forbidden con un mensaje de error diferente.
+Finalmente, llama a la función next para pasar el control al siguiente middleware 
+si no se cumplen ninguna de las condiciones anteriores.
+*/
