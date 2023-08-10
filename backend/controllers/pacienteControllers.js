f729ed70f0d359d83714cd4bc9bc226d30bf74cc
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-09 23:11:40 -0300

    Rutas y controladores de Paciente

---
+++ backend/controllers/pacienteControllers.js
@@ -0,0 +1,14 @@
+import Paciente from "../models/Pacientes.js";
+
+const agregarPaciente = async (req, res) => {
+    
+}
+
+const obtenerPacientes = async (req, res) => {
+    
+}
+
+export {
+    agregarPaciente,
+    obtenerPacientes
+}
