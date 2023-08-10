7dd1b054e3c2f04b00e669684ab3c8dc9b743176
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-09 23:32:05 -0300

    Nuevo paciente

--- backend/controllers/pacienteControllers.js
+++ backend/controllers/pacienteControllers.js
@@ -1,7 +1,12 @@
 import Paciente from "../models/Pacientes.js";
 
 const agregarPaciente = async (req, res) => {
+const paciente = new Paciente(req.body);
+try {
     
+} catch (error) {
+    console.log(error);
+}
 }
 
 const obtenerPacientes = async (req, res) => {
