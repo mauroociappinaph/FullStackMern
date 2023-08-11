ae2ddf2b7332fccdaa4e4edca25ebd30aee73a05
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-10 23:22:50 -0300

    Creación de pacientes

--- backend/controllers/pacienteControllers.js
+++ backend/controllers/pacienteControllers.js
@@ -1,14 +1,15 @@
 import Paciente from "../models/Pacientes.js";
 
 const agregarPaciente = async (req, res) => {
-const paciente = new Paciente(req.body);
-try {
-    
-} catch (error) {
+  const paciente = new Paciente(req.body);
+  paciente.veterinario = req.veterinario._id;
+  try {
+    const pacienteAlmacenado = await paciente.save();
+    res.json(pacienteAlmacenado);
+  } catch (error) {
     console.log(error);
+  }
 }
-}
-
 const obtenerPacientes = async (req, res) => {
     
 }
