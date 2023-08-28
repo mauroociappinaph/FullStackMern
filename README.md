53f7c8b1ab25312523999d5819ffa2579b157ef0
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-08-28 19:06:46 -0300

    Primer Parte de Actualizar paciente

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # FullStackMern
 Proyecto FullStack MERN
 
-MongoDB  - Node js - React 
+MongoDB  - Node js - Express - React 
+
+
+BASE DE DATOS
+-> MONGODB
+-> COMPASS
+-> MONGODB ATLAS
+-> MONGOOSE
+-> POSTMAN
+-> BCRYPT
+BACKEND
+-> NODE JS
+-> EXPRESS
+-> NODEMON
+-> DOTENV
+-> JSON WEB TOKENS
--- backend/controllers/pacienteControllers.js
+++ backend/controllers/pacienteControllers.js
@@ -10,26 +10,37 @@ const agregarPaciente = async (req, res) => {
     console.log(error);
   }
 };
-const obtenerPacientes = async (req, res) => {
-  const paciente = await Paciente.find()
-    .where("veterinario")
-    .equals(req.veterinario);
-  res.json(paciente);
-};
 
 const obtenerPaciente = async (req, res) => {
   console.log(req.params.id);
-  const paciente = await Paciente.findById(req.params.id).where("veterinario");
-  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
-    res.json({ msg: "No autorizado" });
-    return res.status(401).json({ msg: "No autorizado" });
-    if (paciente) {
-      res.json(paciente);
-    }
+  const paciente = await Paciente.findById(req.params.id).where("veterinario", req.veterinario);
+  if (!paciente) {
+    res.status(404).json({ msg: "No encontrado" });
+  } else if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    res.status(401).json({ msg: "No autorizado" });
+  } else {
+    res.json(paciente);
   }
 };
 
-const actualizarPaciente = async (req, res) => {};
+const actualizarPaciente = async (req, res) => {
+  const { id } = req.params;
+  const paciente = await Paciente.findById(id);
+  if (!paciente) {
+    res.status(404).json({ msg: "No encontrado" });
+  } else if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    res.status(401).json({ msg: "Acción no válida" });
+  } else {
+    // Implementar la lógica de actualización aquí
+   paciente.nombre = req.body.nombre;
+   try {
+    const pacienteActualizado = await paciente.save();
+    res.json(pacienteActualizado);
+   } catch (error) {
+    console.log(error);
+   }
+  }
+};
 
 const eliminarPaciente = async (req, res) => {};
 
