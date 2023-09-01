9ca4831f29366b36a80dbece3d21a94adf50bfae
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-09-01 15:29:15 -0300

    Actualizar paciente

--- backend/controllers/pacienteControllers.js
+++ backend/controllers/pacienteControllers.js
@@ -10,37 +10,71 @@ const agregarPaciente = async (req, res) => {
     console.log(error);
   }
 };
+const obtenerPacientes = async (req, res) => {
+  const pacientes = await Paciente.find()
+    .where("veterinario")
+    .equals(req.veterinario);
+
+  res.json(pacientes);
+};
 
 const obtenerPaciente = async (req, res) => {
-  console.log(req.params.id);
-  const paciente = await Paciente.findById(req.params.id).where("veterinario", req.veterinario);
-  if (!paciente) {
-    res.status(404).json({ msg: "No encontrado" });
-  } else if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
-    res.status(401).json({ msg: "No autorizado" });
-  } else {
-    res.json(paciente);
+  try {
+    console.log("ID del paciente (antes de la consulta):", req.params.id);
+    console.log("ID del veterinario (antes de la consulta):", req.veterinario._id);
+
+    const paciente = await Paciente.findById(req.params.id).where(
+      "veterinario",
+      req.veterinario
+    );
+
+    console.log("ID del paciente (después de la consulta):", req.params.id);
+    console.log("ID del veterinario (después de la consulta):", req.veterinario._id);
+
+    if (!paciente) {
+      return res.status(404).json({ msg: "No encontrado" });
+    } else if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+      return res.status(401).json({ msg: "No autorizado" });
+    } else {
+      res.json(paciente);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Error interno del servidor" });
   }
+  console.log("Token recibido:", req.headers.authorization);
+
 };
+
+
 
 const actualizarPaciente = async (req, res) => {
   const { id } = req.params;
   const paciente = await Paciente.findById(id);
+
   if (!paciente) {
-    res.status(404).json({ msg: "No encontrado" });
-  } else if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
-    res.status(401).json({ msg: "Acción no válida" });
-  } else {
-    // Implementar la lógica de actualización aquí
-   paciente.nombre = req.body.nombre;
-   try {
+    return res.status(404).json({ msg: "No Encontrado" });
+  }
+
+  if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+    return res.json({ msg: "Accion no válida" });
+  }
+
+  // Actualizar Paciente
+  paciente.nombre = req.body.nombre || paciente.nombre;
+  paciente.propietario = req.body.propietario || paciente.propietario;
+  paciente.email = req.body.email || paciente.email;
+  paciente.fecha = req.body.fecha || paciente.fecha;
+  paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+  try {
     const pacienteActualizado = await paciente.save();
     res.json(pacienteActualizado);
-   } catch (error) {
+  } catch (error) {
     console.log(error);
-   }
   }
 };
+
 
 const eliminarPaciente = async (req, res) => {};
 
