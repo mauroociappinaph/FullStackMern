76d5490476951ed84238617caa7762c95a44c8a0
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-09-02 11:16:06 -0300

    Diseño de Olvidar Password

--- frontend/src/paginas/OlvidarPassword.jsx
+++ frontend/src/paginas/OlvidarPassword.jsx
@@ -1,14 +1,42 @@
-
+import { Link  } from "react-router-dom";
 
 function OlvidarPassword() {
   return (
     <>
-      <div>
-        1
+     <div>
+        <h1 className="text-orange-400 font-black text-5xl">
+          Olvidé mi contraseña
+        </h1>
       </div>
-      <dir>
-        2
-      </dir>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        <form>
+          <div>
+            <label className="block text-gray-500 font-bold text-xl md:text-left mb-1 md:mb-0 pr-4">
+              Email:
+            </label>
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 mt-3 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500"
+              id="email"
+              type="email"
+              placeholder="correo@correo.com"
+            />
+          </div>
+          <input
+            type="submit"
+            className="bg-orange-400 hover:bg-orange-500 w-full py-2 rounded-md text-white font-bold uppercase m-1 hover:cursor-pointer"
+            value="Recuperar Password"
+          />
+        </form>
+        <nav className="mt-4 lg:flex lg:justify-items-center">
+          <Link
+            to="/"
+            className="block text-center text-gray-400 font-bold text-base md:text-left mb-1 md:mb-0 pr-4"
+          >
+            Inicia sesión
+          </Link>
+         
+        </nav>
+      </div>
     </>
   )
 }
--- frontend/src/paginas/RegistrarCuenta.jsx
+++ frontend/src/paginas/RegistrarCuenta.jsx
@@ -1,11 +1,75 @@
+import { Link } from "react-router-dom";
+
 function Registrar() {
   return (
     <>
       <div>
         <h1 className="text-orange-400 font-black text-5xl   ">
-          Regístrate y Empieza a Administra {" "}
+          Regístrate y Empieza a Administra{" "}
           <span className="text-black"> tus Pacientes </span>
         </h1>
+      </div>
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+        <form>
+          <div>
+            <label className="block text-gray-500 font-bold text-xl md:text-left mb-1 md:mb-0 pr-4">
+              Nombre:
+            </label>
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 mt-3 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500"
+              id="grid-first-name"
+              type="text"
+              placeholder="nombre"
+            />
+          </div>
+          <div>
+            <label className="block text-gray-500 font-bold text-xl md:text-left mb-1 md:mb-0 pr-4">
+              Email:
+            </label>
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 mt-3 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500"
+              id="grid-first-name"
+              type="email"
+              placeholder="correo@correo.com"
+            />
+          </div>
+          <div>
+            <label className="block text-gray-500 font-bold text-xl md:text-left mb-1 md:mb-0 pr-4">
+              Password:
+            </label>
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 mt-3 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500"
+              id="grid-first-name"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div>
+            <label className="block text-gray-500 font-bold text-xl md:text-left mb-1 md:mb-0 pr-4">
+              Repite tu Password:
+            </label>
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 mt-3 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-orange-500"
+              id="grid-first-name"
+              type="password"
+              placeholder="repite tu password"
+            />
+          </div>
+          <input
+            type="submit"
+            className="bg-orange-400 hover:bg-orange-500 w-full py-2 rounded-md text-white font-bold uppercase m-1 hover:cursor-pointer"
+            value="Registrar"
+          />
+        </form>
+        <nav className="mt-4 lg:flex lg:justify-items-center  ">
+          <Link
+            to="/"
+            className="block text-center text-gray-400 font-bold text-base md:text-left mb-1 md:mb-0 pr-4"
+          >
+            Inicia sesión
+          </Link>
+      
+        </nav>
       </div>
     </>
   );
