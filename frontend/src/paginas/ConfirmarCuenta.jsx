3230cc017bfe22015c3ded1198bb13b60f57da73
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-09-09 11:06:19 -0300

    Usuario Confirmado Parte 1

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,10 +1,36 @@
-function ConfirmarCuenta() {
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const ConfirmarCuenta = () => {
+  const params = useParams();
+  const { id } = params;
+
+  useEffect(() => {
+    const confirmarCuenta = async () => {
+      try {
+        const url = `http://localhost:4000/api/veterinarios/confirmar/${id}`;
+        
+        const { data } = await axios(url);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    confirmarCuenta();
+  }, []);
+
   return (
     <>
-      <div>1</div>
-      <div>2</div>
+      <div>
+        <h1 className="text-orange-400 font-black text-5xl   ">
+          Confirmar tu <span className="text-black"> cuenta </span>
+        </h1>
+      </div>
+
+      <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white"></div>
     </>
   );
-}
+};
 
 export default ConfirmarCuenta;
